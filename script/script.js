736c69dc81f7adab856da77b52110813f3400bc1
ssssss-nnnn <90286411+ssssss-nnnn@users.noreply.github.com>
2021-11-08 16:31:53 +0900

    Scriptファイル、works.html追加

---
+++ script/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// ページトップリンク
+
+// #go_upをクリックした際の設定
+$('#go_up').click(function () {
+  $('body,html').animate({
+      scrollTop: 0//ページトップまでスクロール
+  }, 1000);//ページトップスクロールの速さ※数字が大きいほど遅くなる, easingプラグインでアニメーション速度に変化
+  return false;//リンク自体の無効化
+});
