dd427e505e601426963aeb249e1a61d6d4e949e8
ssssss-nnnn <90286411+ssssss-nnnn@users.noreply.github.com>
2021-12-07 18:12:54 +0900

    ハンバーガーメニュー途中まで

--- script/script.js
+++ script/script.js
@@ -9,3 +9,15 @@ $('#go_up').click(function () {
   }, 1500);//ページトップスクロールの速さ※数字が大きいほど遅くなる, easingプラグインでアニメーション速度に変化
   return false;//リンク自体の無効化
 });
+
+
+//ハンバーガーボタン
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
