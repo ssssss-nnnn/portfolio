b6d7689feb657f0da7a0daf9e6f1ac7f064f3c55
ssssss-nnnn <90286411+ssssss-nnnn@users.noreply.github.com>
2022-01-30 19:11:34 +0900

    １００：モーダルウィンドウ途中

--- script/script.js
+++ script/script.js
@@ -21,3 +21,26 @@ $("#g-nav a").click(function () {//ナビゲーションのリンクがクリッ
     $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
     $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
 });
+
+
+// モーダルウィンドウ
+//初回のみモーダルをすぐ出す判定。flagがモーダル表示のstart_open後に代入される
+var access = $.cookie('access')
+if(!access){
+  flag = true;
+  $.cookie('access', false);
+}else{
+  flag = false  
+}
+
+//モーダル表示
+$(".modal-open").modaal({
+start_open:flag, // ページロード時に表示するか
+overlay_close:true,//モーダル背景クリック時に閉じるか
+before_open:function(){// モーダルが開く前に行う動作
+  $('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+},
+after_close:function(){// モーダルが閉じた後に行う動作
+  $('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+}
+});
