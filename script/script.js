f8ddaced6fd3d0eaa67227b6cb2853d70f549f0f
ssssss-nnnn <90286411+ssssss-nnnn@users.noreply.github.com>
2022-02-01 17:14:47 +0900

    100 js書きなおし

--- script/script.js
+++ script/script.js
@@ -24,18 +24,25 @@ $("#g-nav a").click(function () {//ナビゲーションのリンクがクリッ
 
 
 // モーダルウィンドウ
-//初回のみモーダルをすぐ出す判定。flagがモーダル表示のstart_open後に代入される
-var access = $.cookie('access')
-if(!access){
-  flag = true;
-  $.cookie('access', false);
-}else{
-  flag = false  
+
+$('modal-open')on('click', function(event){
+  event.preventDefault();
+  const 
 }
+)
+
+//初回のみモーダルをすぐ出す判定。flagがモーダル表示のstart_open後に代入される
+// var access = $.cookie('access')
+// if(!access){
+//   flag = true;
+//   $.cookie('access', false);
+// }else{
+//   flag = false  
+// }
 
 //モーダル表示
 $(".modal-open").modaal({
-start_open:flag, // ページロード時に表示するか
+// start_open:flag, // ページロード時に表示するか
 overlay_close:true,//モーダル背景クリック時に閉じるか
 before_open:function(){// モーダルが開く前に行う動作
   $('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
