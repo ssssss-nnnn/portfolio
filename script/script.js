b4f9315670796d423931e004af1b13fa22005e8d
ssssss-nnnn <90286411+ssssss-nnnn@users.noreply.github.com>
2022-02-01 22:44:42 +0900

    100モーダル途中まで

--- script/script.js
+++ script/script.js
@@ -25,9 +25,8 @@ $("#g-nav a").click(function () {//ナビゲーションのリンクがクリッ
 
 // モーダルウィンドウ
 
-//画像のモーダル
-$(".modal-open").modaal({
-  type: 'image',
+//テキストを含む一般的なモーダル
+$(".text_box").modaal({
   overlay_close:true,//モーダル背景クリック時に閉じるか
   before_open:function(){// モーダルが開く前に行う動作
     $('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
@@ -36,5 +35,28 @@ $(".modal-open").modaal({
     $('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
   }
 });
+  
+//確認を促すモーダル
+$(".confirm").modaal({
+  type:'confirm',
+  confirm_title: 'ログイン画面',//確認画面タイトル
+  confirm_button_text: 'ログイン', //確認画面ボタンのテキスト
+  confirm_cancel_button_text: 'キャンセル',//確認画面キャンセルボタンのテキスト
+  confirm_content: 'ログインが必要です。<br>この画面はボタンを押さなければ閉じません。',//確認画面の内容
+});
+
+//画像のモーダル
+// $(".modal-open").modaal({
+//   // type: 'image',
+//   overlay_close:true,//モーダル背景クリック時に閉じるか
+//   before_open:function(){// モーダルが開く前に行う動作
+//     $('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+//   },
+//   after_close:function(){// モーダルが閉じた後に行う動作
+//     $('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+//   }
+// });
+
+
 
 
